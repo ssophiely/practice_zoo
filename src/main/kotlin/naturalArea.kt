2c7725d5e8de78665c492b3e6a2398ba31c645ff
Sofia Lakeeva <lakeeva.sofia@yandex.ru>
2023-06-30 17:20:22 +0300

    project's created

---
+++ src/main/kotlin/naturalArea.kt
@@ -0,0 +1,9 @@
+enum class naturalArea(val n: String) {
+    ARCTIC("Арктическая пустыня"),
+    TUNDRA("Тундра"),
+    TAIGA("Тайга"),
+    FSTEPPE("Лесостепь"),
+    TROPIC("Тропический лес"),
+    SAVANNA("Саванна"),
+    DESERT("Пустыня")
+}
